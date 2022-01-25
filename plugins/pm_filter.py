2f703573d61011cda0a6d12499ba0afe7ac067e1
Emily <71280699+Emilyoftg@users.noreply.github.com>
2022-01-25 21:44:11 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -764,7 +764,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         )
     elif query.data == "stats":
         buttons = [[
-            InlineKeyboardButton('⏪️ Back', callback_data='about'),
+            InlineKeyboardButton('⏪️ Back', callback_data='help'),
             InlineKeyboardButton('Refresh 💫', callback_data='rfrsh')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
